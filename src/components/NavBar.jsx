2be3d2590d7fac1d717c2242d3a540e255858dd3
Dmitriy0642 <dmitriy.linnik1337@mail.ru>
2023-12-16 23:33:07 +0100

    creating-business-part

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,19 +1,19 @@
-import React from "react";
-import { useState } from "react";
-import { close, logo, menu } from "../assets";
-import { navLinks } from "../constants";
+import React from 'react'
+import { useState } from 'react'
+import { close, logo, menu } from '../assets'
+import { navLinks } from '../constants'
 
 const Navbar = () => {
-  const [toggle, setToggle] = useState(false);
+  const [toggle, setToggle] = useState(false)
   return (
     <nav className="w-full justify-between flex navbar py-6 items-center">
       <img src={logo} alt="hoobank" className="w-[124px] h-[32px]" />
-      <ul className="sm:flex hidden justify-end items-center flex-1">
+      <ul className="sm:flex hidden justify-end items-center flex-1 cursor-pointer">
         {navLinks.map((nav, index) => (
           <li
             key={nav.id}
             className={`font-poppins font-normal text-white cursor-pointer text-[16px] ${
-              index === navLinks.length - 1 ? "mr-0" : "mr-10"
+              index === navLinks.length - 1 ? 'mr-0' : 'mr-10'
             } `}
           >
             <a href={`${nav.id}`}>{nav.title}</a>
@@ -24,21 +24,21 @@ const Navbar = () => {
         <img
           src={toggle ? close : menu}
           alt="menu"
-          className="w-[28px] h-[28px] object-contain"
+          className="w-[28px] h-[28px] object-contain cursor-pointer"
           onClick={() => setToggle((prev) => !prev)}
         />
       </div>
       <div
         className={`${
-          toggle ? "flex" : "hidden"
+          toggle ? 'flex' : 'hidden'
         } p-6 bg-black-gradient absolute top-20 right-0 mx-4 my-2 min-w-[140px] rounded-xl sidebar`}
       >
-        <ul className=" list-none flex-col justify-end items-center flex-1">
+        <ul className=" list-none flex-col justify-end items-center flex-1 cursor-pointer">
           {navLinks.map((nav, index) => (
             <li
               key={nav.id}
               className={`font-poppins font-normal text-white cursor-pointer text-[16px] ${
-                index === navLinks.length - 1 ? "mr-0" : "mb-4"
+                index === navLinks.length - 1 ? 'mr-0' : 'mb-4'
               } `}
             >
               <a href={`${nav.id}`}>{nav.title}</a>
@@ -47,7 +47,7 @@ const Navbar = () => {
         </ul>
       </div>
     </nav>
-  );
-};
+  )
+}
 
-export default Navbar;
+export default Navbar
