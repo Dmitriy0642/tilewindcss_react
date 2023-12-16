0dbf87aa9a1dccbc21cd2aa5ced2da1d65f38833
Dmitriy0642 <dmitriy.linnik1337@mail.ru>
2023-12-16 21:39:16 +0100

    add-prettier-config

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,7 +1,30 @@
 import React from "react";
+import styles from "../style";
+import {discount, robot} from "../assets";
 
 const Hero = () => {
-  return <div>Hero</div>;
+  return (
+    <section
+      id="home"
+      className={`flex md:flex-row flex-col ${styles.paddingY}`}
+    >
+      <div className={`${styles.flexStart} flex-col xl:px-0 sm:px-16 px-6`}>
+        <div className="flex flex-row item-center py-[6px] bg-discount-gradient rounded-[10px] mb-2">
+          <img src={discount} alt="discount" className="w-[32px] h-[32px]" />
+          <p className={`${styles.paragraph} ml-2`}>
+            <span className="text-white">20%</span>
+            Discount for <span className="text-white">1 Months</span>Account
+          </p>
+        </div>
+        <div className="flex flex-row justify-between items-center w-full">
+          <h1>
+            The Next<br className="sm:block hidden"></br> Genereation Payment
+            Method.
+          </h1>
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default Hero;
