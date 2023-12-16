a13fecf0b3ba97a42cd1d3db70bf599964f4d945
Dmitriy0642 <dmitriy.linnik1337@mail.ru>
2023-12-16 16:44:20 +0100

    create-components

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,19 @@
 import React from "react";
-
+import style from "./style";
 function App() {
   return (
     <div className="bg-primary w-full overflow-hidden">
-      <div className=""></div>
+      <div className={`${style.paddingX} ${style.flexCenter}`}>
+        <div className={`${style.boxWidth}`}>NavBar</div>
+      </div>
+      <div className={`bg-primary ${style.flexStart}`}>
+        <div className={`${style.boxWidth}`}>Hero</div>
+      </div>
+      <div className={`bg-primary ${style.paddingX} ${style.flexStart}`}>
+        <div className={`${style.boxWidth}`}>
+          Stats Business Billin CardDeal Testimoniols Clients CTA Footer
+        </div>
+      </div>
     </div>
   );
 }
